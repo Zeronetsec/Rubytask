f0d29d497cc7b0ac5b015735f6fd1ac0a600b65d
Zeronetsec <senxhunter996@gmail.com>
2026-04-13 10:19:32 +0700

    modified: utils/help.rb

--- utils/help.rb
+++ utils/help.rb
@@ -8,19 +8,19 @@ module Help
     DG = "\033[1;90m"
     CC = "\033[0;36m"
 
-    Name = "rubytask"
-
     def self.show_help
-        puts "#{N}Rubytask command:"
-        puts "    #{R}› #{N}#{Name} #{GG}--create #{DG}<#{CC}name#{DG}> [<#{CC}date#{DG}>]       #{N}- #{YY}create a new task#{N}"
-        puts "    #{R}› #{N}#{Name} #{GG}--list                         #{N}- #{YY}list all tasks#{N}"
-        puts "    #{R}› #{N}#{Name} #{GG}--open #{DG}<#{CC}name#{DG}>                  #{N}- #{YY}open a task#{N}"
-        puts "    #{R}› #{N}#{Name} #{GG}--finish #{DG}<#{CC}name#{DG}>                #{N}- #{YY}finish a task#{N}"
-        puts "    #{R}› #{N}#{Name} #{GG}--delete #{DG}<#{CC}name#{DG}>                #{N}- #{YY}delete a task#{N}"
-        puts "    #{R}› #{N}#{Name} #{GG}--add-note #{DG}<#{CC}name#{DG}>              #{N}- #{YY}add note text to a task#{N}"
-        puts "    #{R}› #{N}#{Name} #{GG}--rename #{DG}<#{CC}name#{DG}> <#{CC}new_name#{DG}>     #{N}- #{YY}rename an existing task#{N}" 
-        puts "    #{R}› #{N}#{Name} #{GG}--version                      #{N}- #{YY}show version#{N}"
-        puts "    #{R}› #{N}#{Name} #{GG}--help                         #{N}- #{YY}show this message#{N}"
+        puts "#{N}Usage: #{GG}rubytask #{CC}<command> [<args>]#{N}"
+
+        puts "#{N}Available command:"
+        puts "    #{R}› #{GG}--create #{DG}<#{CC}name#{DG}> [<#{CC}date#{DG}>]       #{N}- #{YY}create a new task#{N}"
+        puts "    #{R}› #{GG}--open #{DG}<#{CC}name#{DG}>                  #{N}- #{YY}open a task#{N}"
+        puts "    #{R}› #{GG}--finish #{DG}<#{CC}name#{DG}>                #{N}- #{YY}finish a task#{N}"
+        puts "    #{R}› #{GG}--delete #{DG}<#{CC}name#{DG}>                #{N}- #{YY}delete a task#{N}"
+        puts "    #{R}› #{GG}--add-note #{DG}<#{CC}name#{DG}>              #{N}- #{YY}add note text to a task#{N}"
+        puts "    #{R}› #{GG}--rename #{DG}<#{CC}name#{DG}> <#{CC}new_name#{DG}>     #{N}- #{YY}rename an existing task#{N}"
+        puts "    #{R}› #{GG}--list                         #{N}- #{YY}list all tasks#{N}"
+        puts "    #{R}› #{GG}--version                      #{N}- #{YY}show version#{N}"
+        puts "    #{R}› #{GG}--help                         #{N}- #{YY}show this message#{N}"
     end
 end
 
